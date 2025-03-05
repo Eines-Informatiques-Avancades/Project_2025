e02db08cfaba36722f98af2e8e2f75e11f797a78
Ricard Rodríguez <rcard@insicuri.net>
2025-03-05 19:42:32 +0100

    Add a coordinate visualization script
    Create a plot folder where plotting scripts should be placed.
    
    Add a Python script in order to visualize the position of particles in
    space from a data file.

---
+++ plot/visualize_coordinates.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import os
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
+
+# Parse arguments.
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description = "Visualize particles in 3D space from a data file"
+    )
+    parser.add_argument(
+        'filename',
+        type = str,
+        help = "Path to the data file with x, y, z coordinates"
+    )
+    return parser.parse_args()
+
+# Plot the data from the data file.
+def plot_particles(filename):
+    data = np.loadtxt(filename)
+
+    x = data[:, 0]
+    y = data[:, 1]
+    z = data[:, 2]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(
+        111,
+        projection = '3d'
+    )
+
+    ax.scatter(
+        x, y, z,
+        c = 'r',
+        marker = 'o'
+    )
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title('3D Particle Visualization')
+
+    output_filename = os.path.splitext(filename)[0] + '.png'
+    plt.savefig(output_filename)
+
+    print(f"Plot saved as {output_filename}")
+
+if __name__ == '__main__':
+    args = parse_args()
+    plot_particles(args.filename)
