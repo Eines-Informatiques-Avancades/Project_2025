7bc160e195c0ca8c32238c20dbf299c2ddd56f72
Ricard Rodríguez <117524990+ricard-rd@users.noreply.github.com>
2025-03-11 22:56:27 +0100

    Update integrators.f90
    Fix code formatting.

--- include/integrators.f90
+++ include/integrators.f90
@@ -12,15 +12,14 @@ subroutine verlet(part_num, dt, system_size, cutoff, positions, positions_old, f
 
     integer, intent(in) :: part_num
     real, intent(in) :: dt, system_size, cutoff
-    real, allocatable, intent(inout) :: positions(:,:), positions_old(:,:)
-    real, allocatable, intent(out) :: forces(:,:)
-    real, allocatable :: positions_aux(:,:)
-
+    real, allocatable, intent(inout) :: positions(:, :), positions_old(:, :)
+    real, allocatable, intent(out) :: forces(:, :)
+    real, allocatable :: positions_aux(:, :)
 
     allocate(positions(part_num, 3), positions_old(part_num, 3), positions_aux(part_num, 3))
+    
     call compute_forces(part_num, positions, forces, system_size, cutoff)
     positions_aux = positions
     positions = r*positions - positions_old + forces*dt*dt
     positions_old = positions_aux
-
-end subroutine verlet
+end subroutine verlet 
