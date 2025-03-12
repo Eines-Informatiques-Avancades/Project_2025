3a051625e5a4c0dc890f1d9821fdc6e7ad349835
Ricard Rodríguez <rcard@insicuri.net>
2025-03-12 16:07:21 +0100

    read_input.f90: Add input_file as a subroutine argument
    Pass the name of the input file to be read as a subroutine argument
    instead of having to enter it from the terminal screen.
    
    Fix code formatting and rename some variables.
    
    Remove trailing whitespace.

--- include/read_input.f90
+++ include/read_input.f90
@@ -3,50 +3,49 @@
 ! Molecular Dynamics Simulation of a Van der Waals Gas
 ! Itziar Rabal
 !
-! Subroutine that reads external input files that follow the structure of test_input.dat
+! Subroutine that reads external input files that follow the structure of
+! test_input.dat
 !
 
-subroutine read_input(part_num, system_size, lattice_type, timestep, step_num)
+subroutine read_input(input_file, part_num, system_size, lattice_type, timestep, step_num)
     implicit none
+
+    character(70), intent(in) :: input_file
     integer, intent(out) :: part_num, step_num
     real, intent(out) :: system_size, timestep
     character(6), intent(out) :: lattice_type
 
-    character(70) :: filename
-    integer :: io, nlines, i
+    integer :: ios, nlines, i
     character(70), allocatable :: line(:)
-    
+
     nlines = 0
 
-    print*, "enter input file"
-    read(*,*)filename
-
-    open(10, file = filename, status = "old", iostat = io)
-    if (io /= 0) then
-        print*, "error reading input file"
+    open(10, file = input_file, status = 'old', iostat = ios)
+    if (ios /= 0) then
+        print *, 'Error reading input file ', input_file
         stop
     endif
-    
-    do 
-        read(10,*, iostat = io)
-        if (io /= 0) exit
+
+    do
+        read(10, *, iostat = ios)
+        if (ios /= 0) exit
         nlines = nlines + 1
     enddo
-    
+
     rewind(10)
 
     allocate(line(nlines))
 
-    do i =  1, nlines
-        read(10,"(A)")line(i)
+    do i = 1, nlines
+        read(10, '(A)') line(i)
     enddo
 
-    read(line(3)(17:20), "(I3)")part_num
-    read(line(4)(17:20), "(I3)")system_size
-    read(line(5)(17:19), "(A)")lattice_type
-    read(line(6)(17:23), "(F12.6)")timestep
-    read(line(7)(17:23), "(I6)")step_num
-    
+    read(line(3)(17:20), '(I3)') part_num
+    read(line(4)(17:20), '(I3)') system_size
+    read(line(5)(17:19), '(A)') lattice_type
+    read(line(6)(17:23), '(F12.6)') timestep
+    read(line(7)(17:23), '(I6)') step_num
+
     deallocate(line)
 
     close(10)
