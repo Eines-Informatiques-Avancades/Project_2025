20154a6c5cf342b23694b2c5b9170bc568cfc049
Ricard Rodríguez <117524990+ricard-rd@users.noreply.github.com>
2025-03-15 19:28:22 +0100

    Add minor updates to invalid_out_test.sh
    - Add double quoting to variables to prevent globbing and word splitting.
    - Use single quotes in echo commands when possible.
    - Fix indentation (4 spaces instead of 3)
    - In case initial cd fails, abort the test.

--- test/invalid_out_test.sh
+++ test/invalid_out_test.sh
@@ -2,27 +2,33 @@
 
 out_files=("kinetic_energy.dat" "lj_potential.dat" "positions.xyz" "temperature_inst.dat" "total_energy.dat")
 
-cd run_out
+cd run_out || exit
 
-echo ' ' 
-echo "comparing files with test outputs..."
-echo ' ' 
+echo ' '
+echo 'Comparing output files with test outputs...'
+echo ' '
 
 for file in "${out_files[@]}"; do
-   if [ -f $file ]; then
-       echo $file
-       nans=$(grep NaN $file | wc -l)   # check for NaN value
-       echo NaN : '        ' $nans lines
-       infinit=$(grep Infinity $file | wc -l)    # check for Infinity value 
-       echo Infinity : '   ' $infinit lines
-       heads=$(tail -n +2 $file | head -10 | awk '{print $2}')
-       tails=$(tail -10 $file | awk '{print $2}')
-       if [ "$heads" = "$tails" ]; then    # check if all lines display same data
-           echo ERROR : print or propagation issue
-       fi
-   else
-       echo File $file not found.
-   fi
-   echo ' '
-done
+    if [ -f "$file" ]; then
+        echo "$file"
 
+        # Check for NaN values.
+        nans=$(grep NaN "$file" | wc -l)
+        echo "NaN :          $nans lines"
+
+        # Check for Infinity values.
+        infinit=$(grep Infinity "$file" | wc -l)
+        echo "Infinity :     $infinit lines"
+
+        # Check if all the lines in $file display same data.
+        heads=$(tail -n +2 "$file" | head -10 | awk '{print $2}')
+        tails=$(tail -10 "$file" | awk '{print $2}')
+        if [ "$heads" = "$tails" ]; then
+            echo ERROR : print or propagation issue
+        fi
+    else
+        echo "File $file not found."
+    fi
+
+    echo ' '
+done
