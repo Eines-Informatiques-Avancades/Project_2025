b498e7c47078002f460e3cd8834d3882c14f5b87
Ricard Rodríguez <rcard@insicuri.net>
2025-04-02 22:19:21 +0200

    Minor fixes to the temperature plot
    Avoid lines longer than 80 characters. Although no errors would be
    raised, it is always better to avoid long lines.
    
    Fix some spelling mistakes.

--- plot/temperature_evolution.py
+++ plot/temperature_evolution.py
@@ -25,11 +25,14 @@ def read_line_from_file(file_name, line_number):
                 return line
         else:
             file.close()
-            raise ValueError('line %s does not exist in file %s' % (line_number, file_name))
+            raise ValueError(
+                'line %s does not exist in file %s' % (line_number, file_name)
+            )
 
 line = read_line_from_file('../input_parameters.in', 1)
 
-# Function that sets parameters according to atom_type chosen per last simmulation
+# Function that sets parameters according to atom_type chosen per last
+# simulation
 def set_atom_parameters(atom_dictionary, line):
     for i in range(len(atom_dictionary['atom'])):
         if atom_dictionary['atom'][i] == line[:2]:
@@ -37,6 +40,7 @@ def set_atom_parameters(atom_dictionary, line):
             epsilon = atom_dictionary['epsilon'][i]
             sigma = atom_dictionary['sigma'][i]
             return atom, epsilon, sigma
+
 atom = set_atom_parameters(atom_dictionary,line)[0]
 epsilon = set_atom_parameters(atom_dictionary,line)[1]
 sigma = set_atom_parameters(atom_dictionary,line)[2]
@@ -46,13 +50,15 @@ def plot_temperature(data_file, atom, epsilon, label, color):
     data = np.loadtxt(data_file)
 
     # Initialise the subplot function using number of rows and columns
-    figure, axis = plt.subplots(nrows=2, ncols=1, sharex=False, sharey=False, squeeze=True, subplot_kw=None, gridspec_kw=None)
+    figure, axis = plt.subplots(nrows =2, ncols=1,
+                                sharex=False, sharey=False, squeeze=True,
+                                subplot_kw=None, gridspec_kw=None)
     figure.subplots_adjust(hspace=0.5)
-    
+
     # Plot for adimensional data
     axis[0].plot(data[:, 0], data[:, 4], label = label, color = color)
-    axis[0].set_title("Adimensional simmulation")
-    
+    axis[0].set_title("Adimensional simulation")
+
     # Plot using chosen atom parameters
     axis[1].plot(data[:, 0], data[:, 4]*epsilon, label = label, color = color)
     axis[1].set_title(atom + " simulation")
@@ -72,4 +78,3 @@ plt.savefig(output_file, format = 'pdf')
 plt.close()
 
 print(f"Temperature plot saved as {output_file}")
-
