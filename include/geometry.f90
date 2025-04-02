cd68aaefb61aa85a45b616896eb48449b88551c9
Ricard Rodríguez <rcard@insicuri.net>
2025-04-02 22:49:41 +0200

    geometry.f90: Use part_num defined in global_vars
    Instead of determining part_num from the positions array, use the value
    specified in the input_parameters.in file that is then stored as defined
    in the global_vars module.
    
    This will avoid having to determine the size of the positions array
    everytime PBC are applied, improving the program's performance.

--- include/geometry.f90
+++ include/geometry.f90
@@ -20,10 +20,7 @@ module geometry
 
             real, allocatable, intent(inout) :: positions(:)
 
-            integer :: i, part_num
-
-            ! Retrieve the number of particles from the positions array.
-            part_num = size(positions, 1)
+            integer :: i
 
             do i = 1, part_num
                 positions(i) = pbc(positions(i), system_size)
