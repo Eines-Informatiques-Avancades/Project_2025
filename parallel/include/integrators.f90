7970b3994e55f3909ddfff8dfdc51a27a95203b6
Ricard Rodríguez <117524990+ricard-rd@users.noreply.github.com>
2025-04-14 16:25:33 +0200

    Update integrators.f90 code formatting

--- parallel/include/integrators.f90
+++ parallel/include/integrators.f90
@@ -22,11 +22,9 @@ module integrators
 
             real(8), allocatable, intent(inout) :: positions(:, :), velocities(:, :)
             real(8), intent(out) :: lj_potential
-            real(8), allocatable ::  forces(:,:)
-                                    
+            real(8), allocatable ::  forces(:, :)                             
 
-
-!From here it will be moved to the main, and the variables passed as parameters.
+            ! From here it will be moved to the main, and the variables passed as parameters.
             integer :: rank, nproc, ierror, i
             integer, allocatable :: counts(:), displs(:)
 
@@ -36,10 +34,10 @@ module integrators
 
             ! MASTER = 0
 
-            allocate(counts(0:nproc-1))
-            allocate(displs(0:nproc-1))
+            allocate(counts(0:nproc - 1))
+            allocate(displs(0:nproc - 1))
 
-            do i = 0, nproc-1
+            do i = 0, nproc - 1
                 if (i < mod(part_num, nproc)) then
                     counts(i) = (part_num / nproc + 1) 
                 else
@@ -49,23 +47,23 @@ module integrators
             
             displs(0) = 0
 
-            do i = 1, nproc-1
-                displs(i) = displs(i-1) + counts(i-1) 
+            do i = 1, nproc - 1
+                displs(i) = displs(i - 1) + counts(i - 1) 
             end do
-            
     
             call compute_forces(positions, forces, lj_potential)
             do i = displs(rank) + 1, displs(rank) + counts(rank)
                 positions(i,1) = positions(i,1) + velocities(i,1)*timestep + 0.5 * forces(i,1)*timestep*timestep
                 positions(i,2) = positions(i,2) + velocities(i,2)*timestep + 0.5 * forces(i,2)*timestep*timestep
                 positions(i,3) = positions(i,3) + velocities(i,3)*timestep + 0.5 * forces(i,3)*timestep*timestep
+                
                 velocities(i,1) = velocities(i,1) + 0.5 * forces(i,1)*timestep
                 velocities(i,2) = velocities(i,2) + 0.5 * forces(i,2)*timestep
                 velocities(i,3) = velocities(i,3) + 0.5 * forces(i,3)*timestep
             end do
 
             call MPI_Barrier(MPI_COMM_WORLD, ierror)
-            do i = 1,3
+            do i = 1, 3
                 call MPI_Allgatherv(positions(displs(rank) + 1 : displs(rank) + counts(rank), i), counts(rank), MPI_REAL8, &
                                     positions(:,i), counts, displs, MPI_REAL8, MPI_COMM_WORLD, ierror)
             end do
@@ -74,16 +72,14 @@ module integrators
                 call apply_pbc(positions)
             end if
 
-
-
             call compute_forces(positions, forces, lj_potential)
             velocities = velocities + 0.5 * forces*timestep
 
             ! The simulation works without this gathering, but it's better if all the velocities are only divided inside the subroutine.
             ! to work better in the final versions of the code.
-            do i = 1,3
+            do i = 1, 3
                 call MPI_Allgatherv(velocities(displs(rank) + 1 : displs(rank) + counts(rank), i), counts(rank), MPI_REAL8, &
-                                    velocities(:,i), counts, displs, MPI_REAL8, MPI_COMM_WORLD, ierror)
+                                    velocities(:, i), counts, displs, MPI_REAL8, MPI_COMM_WORLD, ierror)
             end do
         end subroutine velocity_verlet
 end module integrators
