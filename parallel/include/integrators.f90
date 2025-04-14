cbb33e5275f6b8b98c8ddbe34574c475a6e75177
Ricard Rodríguez <rcard@insicuri.net>
2025-04-14 17:51:02 +0200

    integrators (parallel): Add use of verlet_list
    Additional changes:
    - Remove trailing whitespace.

--- parallel/include/integrators.f90
+++ parallel/include/integrators.f90
@@ -17,12 +17,14 @@ module integrators
 
     contains
 
-        subroutine velocity_verlet(positions, velocities, lj_potential)
+        subroutine velocity_verlet(positions, velocities, lj_potential, verlet_list)
             implicit none
 
             real(8), allocatable, intent(inout) :: positions(:, :), velocities(:, :)
             real(8), intent(out) :: lj_potential
-            real(8), allocatable ::  forces(:, :)                             
+            real(8), intent(in) :: verlet_list(:, :)
+
+            real(8), allocatable :: forces(:, :)
 
             ! From here it will be moved to the main, and the variables passed as parameters.
             integer :: rank, nproc, ierror, i
@@ -30,7 +32,7 @@ module integrators
 
 
             call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror)
-            call MPI_Comm_size(MPI_COMM_WORLD, nproc, ierror) 
+            call MPI_Comm_size(MPI_COMM_WORLD, nproc, ierror)
 
             ! MASTER = 0
 
@@ -39,24 +41,24 @@ module integrators
 
             do i = 0, nproc - 1
                 if (i < mod(part_num, nproc)) then
-                    counts(i) = (part_num / nproc + 1) 
+                    counts(i) = (part_num / nproc + 1)
                 else
-                    counts(i) = (part_num / nproc) 
+                    counts(i) = (part_num / nproc)
                 end if
             end do
-            
+
             displs(0) = 0
 
             do i = 1, nproc - 1
-                displs(i) = displs(i - 1) + counts(i - 1) 
+                displs(i) = displs(i - 1) + counts(i - 1)
             end do
-    
-            call compute_forces(positions, forces, lj_potential)
+
+            call compute_forces(positions, forces, lj_potential, verlet_list, n_neighbors)
             do i = displs(rank) + 1, displs(rank) + counts(rank)
                 positions(i,1) = positions(i,1) + velocities(i,1)*timestep + 0.5 * forces(i,1)*timestep*timestep
                 positions(i,2) = positions(i,2) + velocities(i,2)*timestep + 0.5 * forces(i,2)*timestep*timestep
                 positions(i,3) = positions(i,3) + velocities(i,3)*timestep + 0.5 * forces(i,3)*timestep*timestep
-                
+
                 velocities(i,1) = velocities(i,1) + 0.5 * forces(i,1)*timestep
                 velocities(i,2) = velocities(i,2) + 0.5 * forces(i,2)*timestep
                 velocities(i,3) = velocities(i,3) + 0.5 * forces(i,3)*timestep
@@ -72,7 +74,7 @@ module integrators
                 call apply_pbc(positions)
             end if
 
-            call compute_forces(positions, forces, lj_potential)
+            call compute_forces(positions, forces, lj_potential, verlet_list, n_neighbors)
             velocities = velocities + 0.5 * forces*timestep
 
             ! The simulation works without this gathering, but it's better if all the velocities are only divided inside the subroutine.
